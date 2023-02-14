955dd891dea59b6fdc7521bbe6019fad9126be4a
Steven Barkley <steven.barkley@outlook.com>
2023-02-14 04:02:44 -0500

    Complete Half of Calculator Project

---
+++ my-app/src/DigitButton.js
@@ -0,0 +1,8 @@
+import {ACTIONS} from "./App";
+
+export default function DigitButton({dispatch, digit}) {
+    return <button onClick={() => dispatch({ type: ACTION.ADD_DIGIT, payload: {digit}})}
+    >
+        {digit}
+    </button>
+}
